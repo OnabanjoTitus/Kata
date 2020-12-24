727901650308a45f2af1fc80668702efcd32438b
chibuzo <daunikman@gmail.com>
2020-12-24 17:56:25 +0100

    Added Bolder font to the display

--- src/com/katasWithGrace/numberUtils/Main.java
+++ src/com/katasWithGrace/numberUtils/Main.java
@@ -10,27 +10,29 @@ public class Main {
         TimeUtil timeUtil = new TimeUtilImpl();
         NumberUtilities numberUtilities = new NumberUtilitiesImpl();
 
+
         //Creating the Frame
         JFrame frame = new JFrame("Number Utilities");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(250, 250);
 
         //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
-        JTextField userInput = new JTextField(10); // accepts upto 10 characters
+        JPanel panel = new JPanel();
+
+        //creating panel components
+        Font displayFont = new Font("Tahoma", Font.BOLD, 19);
+        JLabel label = new JLabel("Enter Time");
+        JTextField userInput = new JTextField(10);
+        userInput.setFont(displayFont);
         JLabel resultLabel = new JLabel("");
+        resultLabel.setFont(displayFont);
+
         JButton accept = new JButton("accept");
         panel.add(label); // Components Added using Flow Layout
         panel.add(userInput);
         panel.add(accept);
         panel.add(resultLabel);
-        userInput.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                resultLabel.setText("");
-            }
-        });
+        userInput.addActionListener(actionEvent -> resultLabel.setText(""));
 
         JPanel optionsPanel = new JPanel(); // the panel is not visible in output
         JRadioButton timeRadio = new JRadioButton("Time");
@@ -41,36 +43,31 @@ public class Main {
         buttonGroup.add(timeRadio);
         optionsPanel.add(numberRadio);
         optionsPanel.add(timeRadio); // Components Added using Flow Layout
-        accept.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                String result ="";
-               if(timeRadio.isSelected()){
-                   try {
-                       result= timeUtil.tellTheTime(userInput.getText());
-                   }
-                   catch (Time.InvalidTimeException invalidTimeException){
-                       result = invalidTimeException.getMessage();
-                   }
+        accept.addActionListener(actionEvent -> {
+            String result = "";
+            if (timeRadio.isSelected()) {
+                try {
+                    result = timeUtil.tellTheTime(userInput.getText());
+                } catch (Time.InvalidTimeException invalidTimeException) {
+                    result = invalidTimeException.getMessage();
+                }
 
-               }
-               else{
-                   try{
-                   result =numberUtilities.convertAnyNumberToWords(userInput.getText());}
-                   catch (NumberUtilitiesImpl.InvalidInputException invalidInputException){
+            } else {
+                try {
+                    result = numberUtilities.convertAnyNumberToWords(userInput.getText());
+                } catch (NumberUtilitiesImpl.InvalidInputException invalidInputException) {
                     result = invalidInputException.getMessage();
-                   }
-               }
-               resultLabel.setText(result);
-
+                }
             }
+            resultLabel.setText(result);
+
         });
 
 
         timeRadio.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                if(timeRadio.isSelected()){
+                if (timeRadio.isSelected()) {
                     label.setText("Enter Time");
 
                 }
@@ -81,7 +78,7 @@ public class Main {
         numberRadio.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                if(numberRadio.isSelected()){
+                if (numberRadio.isSelected()) {
                     label.setText("Enter Number");
 
                 }
@@ -91,52 +88,6 @@ public class Main {
         frame.getContentPane().add(BorderLayout.CENTER, panel);
         frame.getContentPane().add(BorderLayout.NORTH, optionsPanel);
         frame.setVisible(true);
-
-
-
-
-
-
-
-//
-////        displayOutput("Welcome to our Application");
-//
-//        int input= Integer.parseInt(collectInputs("Type 1 for Time to Word\nType 2 for Number to word",displayBoard));
-//        switch (input){
-//            case 1:
-//                runTimeToWordApplication(displayBoard);
-//                break;
-//            case 2:
-//                runNumberToWordApplication(displayBoard);
-//                break;
-//            default:
-//                displayOutput("Invalid input");
-//                System.exit(0);
-//                break;
-//        }
-
+       }
     }
 
-    public static String collectInputs(String promptMessage, JFrame component) {
-        return JOptionPane.showInputDialog(component, promptMessage);
-    }
-    public static void runTimeToWordApplication(JFrame jFrame) {
-        TimeUtil timeUtil = new TimeUtilImpl();
-        String time = collectInputs("Enter Time", jFrame);
-        displayOutput(timeUtil.tellTheTime(time));
-
-    }
-    public static void runNumberToWordApplication(JFrame jFrame) {
-        NumberUtilities numberUtilities = new NumberUtilitiesImpl();
-        String userInput = collectInputs("Enter number to convert to word", jFrame);
-        displayOutput(numberUtilities.convertAnyNumberToWords(userInput));
-
-    }
-
-    public static void  displayOutput(String messageToOutput){
-        System.out.println(messageToOutput);
-//        JOptionPane.showMessageDialog(null, messageToOutput);
-
-    }
-
-}
