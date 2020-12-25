219b45bdd3c5592d257e29e05d02388a9a394152
chibuzo <daunikman@gmail.com>
2020-12-25 06:03:16 +0100

    Introduced conditional rendering

--- src/com/katasWithGrace/numberUtils/Main.java
+++ src/com/katasWithGrace/numberUtils/Main.java
@@ -4,89 +4,123 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
+    private static final TimeUtil timeUtil = new TimeUtilImpl();
+    private  static final NumberUtilities numberUtilities = new NumberUtilitiesImpl();
+    private static final JFrame frame = new JFrame("Number to Word Converter");
+    private static final JPanel panel = new JPanel();
+    private static final JLabel titleLabel = new JLabel("Choose Input type");
+    private static final Font displayFont = new Font("Tahoma", Font.BOLD, 22);
+    private static final Font componentFont = new Font("Tahoma", Font.BOLD, 14);
+    private static  final Font resultFont = new Font("Tahoma", Font.BOLD, 30);
+    private static  final JLabel labelForUserInputTextField = new JLabel("Enter Number");
+    private static final  JTextField userInputTextField = new JTextField(20);
+   private static final JLabel userResultLabel = new JLabel("");
+    private static final JButton clickMeButton = new JButton("CLICK ME");
+    private static final JPanel optionsPanel = new JPanel(); // the panel is not visible in output
+    private static final JRadioButton timeRadio = new JRadioButton("Time");
+    private static final JRadioButton numberRadio = new JRadioButton("Number");
+
+
+
     public static void main(String[] args) {
-        TimeUtil timeUtil = new TimeUtilImpl();
-        NumberUtilities numberUtilities = new NumberUtilitiesImpl();
 
+        initialRender();
+        renderNumberDisplay();
 
-        //Creating the Frame
-        JFrame frame = new JFrame("Number Utilities");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(720, 600);
-
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel();
-
-        //creating panel components
-        Font displayFont = new Font("Tahoma", Font.BOLD, 19);
-        JLabel label = new JLabel("Enter Time");
-        JTextField userInput = new JTextField(25);
-        userInput.setFont(displayFont);
-        JLabel resultLabel = new JLabel("");
-        resultLabel.setBounds(2,4,220,900);
-        resultLabel.setFont(displayFont);
-
-        JButton accept = new JButton("accept");
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(userInput);
-        panel.add(accept);
-        panel.add(resultLabel);
-        userInput.addActionListener(actionEvent -> resultLabel.setText(""));
-
-        JPanel optionsPanel = new JPanel(); // the panel is not visible in output
-        JRadioButton timeRadio = new JRadioButton("Time");
-        timeRadio.setSelected(true);
-        JRadioButton numberRadio = new JRadioButton("Number");
-        ButtonGroup buttonGroup = new ButtonGroup();
-        buttonGroup.add(numberRadio);
-        buttonGroup.add(timeRadio);
-        optionsPanel.add(numberRadio);
-        optionsPanel.add(timeRadio); // Components Added using Flow Layout
-        accept.addActionListener(actionEvent -> {
-            String result = "";
-            if (timeRadio.isSelected()) {
-                try {
-                   result = timeUtil.tellTheTime(userInput.getText());
-                   result= String.format("<html><div style=\"width:%dpx;\">%s</div></html>", 500, result);
-                } catch (Time.InvalidTimeException invalidTimeException) {
-                    result = invalidTimeException.getMessage();
-                }
-
-            } else {
-                try {
-
-
-                    result = numberUtilities.convertAnyNumberToWords(userInput.getText());
-                    result = String.format("<html><div style=\"width:%dpx;\">%s</div></html>", 500, result);
-                } catch (NumberUtilitiesImpl.InvalidInputException invalidInputException) {
-                    result = invalidInputException.getMessage();
-                }
-            }
-            resultLabel.setText(result);
-
-        });
-
-
-        timeRadio.addActionListener(actionEvent -> {
-            if (timeRadio.isSelected()) {
-                label.setText("Enter Time");
-                resultLabel.setText("");
-
-            }
-
-        });
-
-        numberRadio.addActionListener(actionEvent -> {
-            if (numberRadio.isSelected()) {
-                label.setText("Enter Number");
-                resultLabel.setText("");
-
-            }
-        });
-        //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.CENTER, panel);
-        frame.getContentPane().add(BorderLayout.NORTH, optionsPanel);
-        frame.setVisible(true);
        }
+       public static void initialRender(){
+           frame.setSize(720, 600);
+           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           //creating panel components
+           titleLabel.setFont(componentFont);
+           userInputTextField.setFont(displayFont);
+
+
+
+           clickMeButton.setFont(componentFont);
+           labelForUserInputTextField.setFont(componentFont);
+           panel.add(labelForUserInputTextField); // Components Added using Flow Layout
+           panel.add(userInputTextField);
+           panel.add(clickMeButton);
+           panel.add(userResultLabel);
+           userInputTextField.addActionListener(actionEvent -> userResultLabel.setText(""));
+
+
+           timeRadio.setFont(componentFont);
+           numberRadio.setFont(componentFont);
+           numberRadio.setSelected(true);
+           ButtonGroup buttonGroup = new ButtonGroup();
+
+           buttonGroup.add(numberRadio);
+           buttonGroup.add(timeRadio);
+           optionsPanel.add(numberRadio);
+           optionsPanel.add(timeRadio); // Components Added using Flow Layout
+           clickMeButton.addActionListener(actionEvent -> {
+               String result;
+               if (timeRadio.isSelected()) {
+                   try {
+                       result = timeUtil.tellTheTime(userInputTextField.getText());
+                       result= String.format("<html><div style=\"width:%dpx;\">%s</div></html>", 500, result);
+                   } catch (Time.InvalidTimeException invalidTimeException) {
+                       result = invalidTimeException.getMessage();
+                   }
+
+               } else {
+                   try {
+
+
+                       result = numberUtilities.convertAnyNumberToWords(userInputTextField.getText());
+                       result = String.format("<html><div style=\"width:%dpx;\">%s</div></html>", 500, result);
+                   } catch (NumberUtilitiesImpl.InvalidInputException invalidInputException) {
+                       result = invalidInputException.getMessage();
+                   }
+               }
+               userResultLabel.setText(result);
+
+           });
+
+
+           timeRadio.addActionListener(actionEvent -> {
+               if (timeRadio.isSelected()) {
+                   renderTimeDisplay();
+               }
+           });
+
+           numberRadio.addActionListener(actionEvent -> {
+               if (numberRadio.isSelected()) {
+                   renderNumberDisplay();
+
+
+
+               }
+           });
+
+           frame.getContentPane().add(BorderLayout.CENTER, panel);
+           frame.getContentPane().add(BorderLayout.NORTH, optionsPanel);
+           frame.setVisible(true);
+
+       }
+
+       public static void renderTimeDisplay(){
+           labelForUserInputTextField.setText("Enter Time");
+           userInputTextField.setColumns(5);
+           userResultLabel.setText("");
+           frame.setSize(350   , 350);
+           userResultLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+           userResultLabel.setBounds(2,4,200,200);
+
+
+       }
+
+    public static void renderNumberDisplay(){
+        labelForUserInputTextField.setText("Enter Number");
+        userInputTextField.setColumns(20);
+        userResultLabel.setText("");
+        frame.setSize(720, 600);
+        userResultLabel.setBounds(2,4,220,900);
+        userResultLabel.setFont(resultFont);
+
+        userResultLabel.setFont(displayFont);
+    }
     }
 
