8153f2fa0ef67eefcb7f8a23d455a3057b3a432b
chibuzo <daunikman@gmail.com>
2020-12-25 06:24:57 +0100

    what is the current time added

--- src/com/katasWithGrace/numberUtils/Main.java
+++ src/com/katasWithGrace/numberUtils/Main.java
@@ -6,9 +6,8 @@ import java.awt.*;
 public class Main {
     private static final TimeUtil timeUtil = new TimeUtilImpl();
     private  static final NumberUtilities numberUtilities = new NumberUtilitiesImpl();
-    private static final JFrame frame = new JFrame("Number to Word Converter");
+    private static final JFrame frame = new JFrame();
     private static final JPanel panel = new JPanel();
-    private static final JLabel titleLabel = new JLabel("Choose Input type");
     private static final Font displayFont = new Font("Tahoma", Font.BOLD, 22);
     private static final Font componentFont = new Font("Tahoma", Font.BOLD, 14);
     private static  final Font resultFont = new Font("Tahoma", Font.BOLD, 30);
@@ -17,8 +16,11 @@ public class Main {
    private static final JLabel userResultLabel = new JLabel("");
     private static final JButton clickMeButton = new JButton("CLICK ME");
     private static final JPanel optionsPanel = new JPanel(); // the panel is not visible in output
-    private static final JRadioButton timeRadio = new JRadioButton("Time");
-    private static final JRadioButton numberRadio = new JRadioButton("Number");
+    private static final JRadioButton timeRadio = new JRadioButton("What is the Time Application");
+    private static final JRadioButton numberRadio = new JRadioButton("Number to Word Application");
+    private static final JRadioButton whatIsTheTime = new JRadioButton("What is the current Time?");
+
+    private static final ButtonGroup buttonGroup = new ButtonGroup();
 
 
 
@@ -32,7 +34,6 @@ public class Main {
            frame.setSize(720, 600);
            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
            //creating panel components
-           titleLabel.setFont(componentFont);
            userInputTextField.setFont(displayFont);
 
 
@@ -43,20 +44,19 @@ public class Main {
            panel.add(userInputTextField);
            panel.add(clickMeButton);
            panel.add(userResultLabel);
-           userInputTextField.addActionListener(actionEvent -> userResultLabel.setText(""));
-
-
            timeRadio.setFont(componentFont);
            numberRadio.setFont(componentFont);
+           whatIsTheTime.setFont(componentFont);
            numberRadio.setSelected(true);
-           ButtonGroup buttonGroup = new ButtonGroup();
 
            buttonGroup.add(numberRadio);
            buttonGroup.add(timeRadio);
+           buttonGroup.add(whatIsTheTime);
            optionsPanel.add(numberRadio);
+           optionsPanel.add(whatIsTheTime);
            optionsPanel.add(timeRadio); // Components Added using Flow Layout
            clickMeButton.addActionListener(actionEvent -> {
-               String result;
+               String result = null;
                if (timeRadio.isSelected()) {
                    try {
                        result = timeUtil.tellTheTime(userInputTextField.getText());
@@ -65,7 +65,12 @@ public class Main {
                        result = invalidTimeException.getMessage();
                    }
 
-               } else {
+               }
+               if(whatIsTheTime.isSelected()){
+                   result = timeUtil.greetUser() +"\n";
+                   result =result+ "\n"+ timeUtil.whatIsTheCurrentTime();
+               }
+               if(numberRadio.isSelected()){
                    try {
 
 
@@ -89,9 +94,12 @@ public class Main {
            numberRadio.addActionListener(actionEvent -> {
                if (numberRadio.isSelected()) {
                    renderNumberDisplay();
+               }
+           });
 
-
-
+           whatIsTheTime.addActionListener(actionEvent -> {
+               if(whatIsTheTime.isSelected()){
+                   renderWhatIsTheTimeDisplay();
                }
            });
 
@@ -101,26 +109,52 @@ public class Main {
 
        }
 
-       public static void renderTimeDisplay(){
+    private static void renderWhatIsTheTimeDisplay() {
+        labelForUserInputTextField.setText("Enter Time");
+        labelForUserInputTextField.setVisible(false);
+        clickMeButton.setText("What is the Time?");
+        userInputTextField.setVisible(false);
+        frame.setTitle("Clock Application");
+        userInputTextField.setColumns(5);
+        userResultLabel.setText("");
+        frame.setSize(350   , 350);
+        userResultLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+        userResultLabel.setBounds(2,4,200,200);
+        timeRadio.setVisible(true);
+        whatIsTheTime.setVisible(false);
+        numberRadio.setVisible(true);
+    }
+
+    public static void renderTimeDisplay(){
            labelForUserInputTextField.setText("Enter Time");
+           frame.setTitle("What is the Time Application");
            userInputTextField.setColumns(5);
+           userInputTextField.setVisible(true);
            userResultLabel.setText("");
            frame.setSize(350   , 350);
            userResultLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
            userResultLabel.setBounds(2,4,200,200);
+           timeRadio.setVisible(false);
+           whatIsTheTime.setVisible(true);
+           numberRadio.setVisible(true);
 
 
        }
 
     public static void renderNumberDisplay(){
+        frame.setTitle("Numbers to Words Application");
         labelForUserInputTextField.setText("Enter Number");
         userInputTextField.setColumns(20);
+        userInputTextField.setVisible(true);
         userResultLabel.setText("");
         frame.setSize(720, 600);
         userResultLabel.setBounds(2,4,220,900);
         userResultLabel.setFont(resultFont);
 
         userResultLabel.setFont(displayFont);
+        timeRadio.setVisible(true);
+        whatIsTheTime.setVisible(true);
+        numberRadio.setVisible(false);
     }
     }
 
